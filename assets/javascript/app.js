69518f0000e3bc38343b0f824988f8d70fd532d7
Mr_Cheese <kevinprovost776@gmail.com>
2019-01-19 00:18:23 -0600

    onclick double run from button element in html dom fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,29 +2,31 @@ $(document).ready(function(){
 console.log("Ready");
 
 //Establishing array with strings for the topic to be sentt to the API
-var movies = ["Shrek", "Cinderella","Armageddon","Gladiator","Bullet","Cars","Alien","Click","Casino"];
+var movies = ["Shrek", "Cinderella","Armageddon","Gladiator","Cars","Aliens","Godfather"];
 console.log(movies)
 //Declaring text variable to hold the element text
 var text = "<div>"
 //callback function to display each string individually into a button
 movies.forEach(display);
-console.log(display)
-text += "</div";
+
+text += "</div>";
 //inserting each string into the dom
-document.getElementById("buttons").innerHTML = text += movies;
+document.getElementById("buttons").innerHTML = text;
 
 function display(value) {
-    text += "<button class='btn-lg btn-primary' movie-data=" + value + ">" + value + "</button>";
+    text += "<button class='btn-lg btn-primary' movie-data=" + value + ">" + value +"</button>";
 }
-
+console.log(text)
 $("button").on("click", function(){
+    
+    
     var mdata = $(this).attr("movie-data");
     var apiKey = "N5OKJJ5Kx7gKlaYEZha1x3zZvZli3Wwd";
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + mdata + "&api_key=" + apiKey + "&limit=10";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + mdata + "&api_key=" + apiKey + "&limit=20";
     var xhr = $.get(queryURL);
     xhr.done(function(data) { console.log("success got data", data); 
     console.log(data.data)
-    
+    console.log(xhr)
     var results = data.data;
 
     for (var i = 0; i < results.length; i++) {
@@ -34,8 +36,7 @@ $("button").on("click", function(){
         var gifImg = $("<img>");
         gifImg.attr("src", url);
 
-        $("#content-here").append(gifImg);
-
+        $("#content-here").prepend(gifImg);
     }
     
     
