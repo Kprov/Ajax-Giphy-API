b5b3ad7bfad08cea8c2b405dfbc7dba42507b40f
Mr_Cheese <kevinprovost776@gmail.com>
2019-01-18 22:38:15 -0600

    giphy API successful, still working out minor kinks and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 console.log("Ready");
 
 //Establishing array with strings for the topic to be sentt to the API
-var movies = ["star wars", "indiana jones","pulp fiction","the sting","bullet","cars","the aviator"];
+var movies = ["Shrek", "Cinderella","Armageddon","Gladiator","Bullet","Cars","Alien","Click","Casino"];
 console.log(movies)
 //Declaring text variable to hold the element text
 var text = "<div>"
@@ -20,11 +20,40 @@ function display(value) {
 $("button").on("click", function(){
     var mdata = $(this).attr("movie-data");
     var apiKey = "N5OKJJ5Kx7gKlaYEZha1x3zZvZli3Wwd";
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + mdata + "&api_key=" + apiKey + "&limit=5";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + mdata + "&api_key=" + apiKey + "&limit=10";
     var xhr = $.get(queryURL);
-    xhr.done(function(data) { console.log("success got data", data); });
+    xhr.done(function(data) { console.log("success got data", data); 
+    console.log(data.data)
+    
+    var results = data.data;
 
-    document.getElementById("content-here").innerHTML = xhr;
+    for (var i = 0; i < results.length; i++) {
+        var url = results[i].images.fixed_height.url;
+        console.log(url)
+
+        var gifImg = $("<img>");
+        gifImg.attr("src", url);
+
+        $("#content-here").append(gifImg);
+
+    }
+    
+    
+});
+    
+
+    
+
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    //   }).then(function(response) {
+    //       var response = response.data
+
+    
+    
+    
+    
     //AJAX initializing giphy API
 })
 
